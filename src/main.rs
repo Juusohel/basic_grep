7180736bb362a0c3e86a35ee70866980e56e004e
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-02-11 16:27:57 +0200

    added basic commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); //collecting commandline arguments to Vec
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {} in {}", query, filename)
+
 }
