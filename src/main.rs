0212ba3c3d704945a53cc84a874f06a58bd364f3
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-02-13 00:00:17 +0200

    Added basic error handling for not enough arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::env;
+use std::{env, process};
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); //collecting commandline arguments to Vec
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem with arguments provided: {}", err);
+        process::exit(1)
+    });
 
     println!("Searching for {} in {}", config.query, config.filename);
 
@@ -19,11 +22,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename}
+        Ok(Config { query, filename})
     }
 
 }
