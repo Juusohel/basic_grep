52ac55e92bb02536b2f169008584fcc8819f42d4
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-02-12 22:23:16 +0200

    Added actually reading a file and printing to commandline

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); //collecting commandline arguments to Vec
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for {} in {}", query, filename)
+    println!("Searching for {} in {}", query, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Error reading the file"); //reading file
+
+    println!("Text: \n{}", contents);
 
 }
