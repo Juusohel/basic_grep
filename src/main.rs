2512329744499e0abc07c07dccc246a60bcb63dc
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-02-12 23:40:04 +0200

    Refactored config to its own function returning a struct instead.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); //collecting commandline arguments to Vec
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {} in {}", query, filename);
+    println!("Searching for {} in {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Error reading the file"); //reading file
 
     println!("Text: \n{}", contents);
-
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename}
+}
+
