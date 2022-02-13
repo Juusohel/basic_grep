7dc8fb835abdcc42264fe68fcac53dd878b82df7
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-02-13 02:21:01 +0200

    Moved functionality to lib.rs to clear main.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{env, process};
-use std::error::Error;
-use std::fs;
+use basic_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); //collecting commandline arguments to Vec
@@ -11,36 +10,10 @@ fn main() {
 
     println!("Searching for {} in {}", config.query, config.filename);
 
-    if let Err(e) = run(config){
+    if let Err(e) = basic_grep::run(config){
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("Text: \n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename})
-    }
-
 }
 
 
