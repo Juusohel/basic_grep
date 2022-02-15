755aa6d49a961c5218c75fc48069284a036a3b72
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-02-15 22:05:35 +0200

    Cleaned up error handling

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use basic_grep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect(); //collecting commandline arguments to Vec
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem with arguments provided: {}", err);
+        eprintln!("Problem with arguments provided: {}", err);
         process::exit(1)
     });
 
 
     if let Err(e) = basic_grep::run(config){
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
